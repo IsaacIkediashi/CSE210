6f8b16e11b88dc8bb8d5c8a2c0b921457de6e6ff
IsaacIkediashi <isaacikediashi192@gmail.com>
2026-05-28 22:13:04 +0100

    my fraction code for week 03

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+      Fraction fraction = new Fraction();
+      Fraction fraction1 = new Fraction(6);
+      Fraction fraction2 = new Fraction(6, 7);
+      
+      fraction.SetTop(3);
+      fraction.SetBottom(4);
+
+      Console.WriteLine(fraction.GetTop());
+      Console.WriteLine(fraction.GetBottom());
+
+      Console.WriteLine(fraction.GetFractionString());
+      Console.WriteLine(fraction.GetDecimalValue());
+
+
+      Console.WriteLine(fraction1.GetFractionString());
+      Console.WriteLine(fraction1.GetDecimalValue());
+
+      Console.WriteLine(fraction2.GetFractionString());
+      Console.WriteLine(fraction2.GetDecimalValue());
     }
 }
